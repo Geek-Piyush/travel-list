01e87354dddea94d1454e963ac79e60de92477af
Piyush Nashikkar <piyushnas74@gmail.com>
2025-05-10 10:13:54 +0530

    Added - Add, delete and Check mark function by lifting state up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
-
 export default function App() {
+  const [items, setItem] = useState([]);
+
+  function handleAddItems(item) {
+    setItem((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItem((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItem((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackageList />
+      <Form handleAddItems={handleAddItems} />
+      <PackageList
+        items={items}
+        handleDeleteItem={handleDeleteItem}
+        handleToggleItem={handleToggleItem}
+      />
       <Stats />
     </div>
   );
@@ -20,15 +36,17 @@ function Logo() {
   return <h1> 🌴Far Away💼</h1>;
 }
 
-function Form() {
+function Form({ handleAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
 
+    handleAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -55,24 +73,34 @@ function Form() {
     </form>
   );
 }
-function PackageList() {
+function PackageList({ items, handleDeleteItem, handleToggleItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            handleDeleteItem={handleDeleteItem}
+            handleToggleItem={handleToggleItem}
+          />
         ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, handleDeleteItem, handleToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => handleToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
